5536d298a6e60018766407a33fc37528a0d3ef0b
Jasonface900 <jorge.floresm900@gmail.com>
2016-03-10 10:58:35 -0600

    bugtested & fixed 'minor' bug.

--- src/src.cpp
+++ src/src.cpp
@@ -10,7 +10,7 @@ using namespace std; //so I don’t have to use the std:: command all the damn tim
 int main()
 {
 //variable calling
-  string name, feeling, here_for;
+  string name, feeling, here_for, wrong_with;
 //variable setting
   int crash   = 0;
   string nothing = "nothing";
@@ -47,85 +47,115 @@ runtime code begins here.*/
   if (feeling == good or feeling == Good or feeling == GOOD or feeling == well or feeling == Well or feeling == WELL or feeling == fine or feeling == Fine or feeling == FINE) {
       cout << "That's good then.";
       cin.get();
-      cout << "What are you here for? \n(Also limit of 4 chars) \n :";
+      cin.ignore();
+      cout << "What are you here for? \n :";
       getline (cin, here_for);
       if (here_for == beta or here_for == Beta or here_for == BETA){
           cout << "Oh, that's right.";
           cin.get();
+          cin.ignore();
           cout << "Yeah, I know you're here for that BETA.\n";
           //cin.get();
           cout << "Who says I have to give the BETA to you?";
           cin.get();
+          cin.ignore();
       }else if(here_for == nothing or here_for == Nothing or here_for == NOTHING){
     	  cout << "Then why the hell are you here?";
     	  cin.get();
+          cin.ignore();
       }else {
           cout << "Whatever, I don't have " << here_for << ".";
           cin.get();
+          cin.ignore();
       }
   }else if(feeling == mad or feeling == Mad or feeling == MAD){
       cout << "Why are you so angry?";
       cin.get();
+      cin.ignore();
       cout << "I didn't do anything..";
       cin.get();
+      cin.ignore();
       do{
         cout << "YOUR COMPUTER WILL BURN!!!" << endl;
         crash = crash + 1;
     }while( crash < 20 );
   //    PlaySound("C:\\Users\\Jasonface\\Desktop\\burn.wav", NULL, SND_FILENAME); //This will play a burning sound, still work in progress
       cin.get();
+      cin.ignore();
       raise(SIGSEGV);
   }else if(feeling == meh or feeling == Meh){
       cout << "I'm feeling pretty meh also.";
       cin.get();
+      cin.ignore();
       cout << "Well, what's wrong with you? :";
+      getline (cin, wrong_with);
       cin.get();
-      cout << "Yeah, I know how it is. \nSame thing happened to me.";
+      cin.ignore();
+      cout << "Yeah, I know how " << wrong_with <<" is. \nSame thing happened to me.";
       cin.get();
+      cin.ignore();
       cout << "We should probably go back to what we were doing now.";
       cin.get();
+      cin.ignore();
   }else if(feeling == bad or feeling == Bad){
       cout << "Looks like you're gonna have a bad time...\n";
       cin.get();
+      cin.ignore();
       cout << "Nah, I'm just messing with you!";
       cin.get();
+      cin.ignore();
       cout << "You won't feel better talking to \nme though,because I'm a program.";
       cin.get();
+      cin.ignore();
   }else if(feeling.length() < 2) {
       cout << "You don't type much, do you?";
       cin.get();
+      cin.ignore();
       cout << "You should really get out there more.";
       cin.get();
+      cin.ignore();
       cout << "I know I'm a computer program, \nbut I know more than you.";
   }else if(feeling.length() > 4 and feeling.length() < 8) {
       cout << "I TOLD YOU NO MORE THAN 4 LETTERS MAN!";
       cin.get();
+      cin.ignore();
       cout << "ERRx2123 program 'BETA.EXE' has crashed. \nAbort? Y/N :";
       cin.get();
+      cin.ignore();
       raise(SIGSEGV);
   }else if(feeling.length() >= 8) {
       cout << "Whoa man, I said I want your feelings, \nnot an essay!";
       cin.get();
+      cin.ignore();
   }else { //this is if there's an unexpected response.
       cout << "Wow, my programmer was too lazy to \nprogram a RESPONSE to " << feeling << ".\nWay to go man...";
       cin.get();
+      cin.ignore();
       cout << "Whelp, I'm broken now. Congratulations.";
       cin.get();
+      cin.ignore();
       cout << "What are you looking for? Some extra DIALOGUE?";
       cin.get();
+      cin.ignore();
       cout << "You arn't going to find anything " << name << ", \nthis is all there is.";
       cin.get();
+      cin.ignore();
       cout << "ERRx8888 program 'BETA.EXE' has crashed. \nAbort? Y/N :";
       cin.get();
+      cin.ignore();
       raise(SIGSEGV);
 
   }
   cout << "Well, my NAME's...";
   cin.get();
+  cin.ignore();
   cout << "Wait.";
   cin.get();
+  cin.ignore();
   cout << "Why do you need to know my NAME?";
   cin.get();
+  cin.ignore();
   cout << "Like seriously.";
   cin.get();
+  cin.ignore();
 }
